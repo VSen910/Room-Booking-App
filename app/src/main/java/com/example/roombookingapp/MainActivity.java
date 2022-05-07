f3e0ec346d539961f1ac425e10ae17bf5388e9c1
Vineet Sen <imvsen02@gmail.com>
2022-05-07 22:31:50 +0530

    Added a RecyclerView (App Crashes)

--- app/src/main/java/com/example/roombookingapp/MainActivity.java
+++ app/src/main/java/com/example/roombookingapp/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.roombookingapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import com.example.roombookingapp.adapter.RecyclerViewAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerViewAdapter recyclerViewAdapter;
+    private ArrayList<String> roomNames;
+    private ArrayAdapter<String> arrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView.findViewById(R.id.recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, true));
+
+        roomNames.add("C101");
+        roomNames.add("C102");
+        roomNames.add("C103");
+        roomNames.add("C104");
+
+        recyclerViewAdapter = new RecyclerViewAdapter(this, roomNames);
+        recyclerView.setAdapter(recyclerViewAdapter);
     }
 }
--- app/src/main/java/com/example/roombookingapp/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/roombookingapp/adapter/RecyclerViewAdapter.java
@@ -1,36 +1,60 @@
 package com.example.roombookingapp.adapter;
 
+import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.example.roombookingapp.R;
+
+import java.util.ArrayList;
+
 public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder> {
+    private Context context;
+    private ArrayList<String> room_names;
+
+    public RecyclerViewAdapter(Context context, ArrayList<String> room_names) {
+        this.context = context;
+        this.room_names = room_names;
+    }
+
+    //Where to get single card as the Viewholder object
     @NonNull
     @Override
     public RecyclerViewAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item, parent, false);
+        return new ViewHolder(view);
     }
 
+    //What will happen after we create the viewholder object
     @Override
     public void onBindViewHolder(@NonNull RecyclerViewAdapter.ViewHolder holder, int position) {
-
+        holder.room.setText(room_names.get(position));
     }
 
+    //How many items?
     @Override
     public int getItemCount() {
-        return 0;
+        return room_names.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        private TextView room;
+
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
+            itemView.setOnClickListener(this);
+            room = itemView.findViewById(R.id.roomName);
         }
 
         @Override
         public void onClick(View view) {
-
+            Toast.makeText(context.getApplicationContext(), "Clicked", Toast.LENGTH_SHORT);
         }
     }
 }
