9ad2b29382e1e9094cc6670fee6681452dffe6e3
Vineet Sen <imvsen02@gmail.com>
2022-05-11 00:04:56 +0530

    Created a basic activity for time slots screen

---
+++ app/src/main/java/com/example/roombookingapp/TimeSlotActivity.java
@@ -0,0 +1,14 @@
+package com.example.roombookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TimeSlotActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_time_slot);
+    }
+}
