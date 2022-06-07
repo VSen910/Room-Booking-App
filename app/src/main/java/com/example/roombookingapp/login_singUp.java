a4d67e699efe3fab70dda502cea4c450e6789d00
Palash Bhasme <palashbhasme123@gmail.com>
2022-06-07 22:59:03 +0530

    Created login and signup front page

---
+++ app/src/main/java/com/example/roombookingapp/login_singUp.java
@@ -0,0 +1,13 @@
+package com.example.roombookingapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class login_singUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_sing_up);
+    }
+}
