0e52fadf2343542a9da89f18cc516aceba28287c
Palash Bhasme <palashbhasme123@gmail.com>
2022-07-15 16:02:58 +0530

    Connected purpose view to all the time slots.

--- app/src/main/java/com/example/roombookingapp/TimeSlotActivity.java
+++ app/src/main/java/com/example/roombookingapp/TimeSlotActivity.java
@@ -14,6 +14,13 @@ public class TimeSlotActivity extends AppCompatActivity implements View.OnClickL
 
     private TextView roomnum;
     private Button ts1;
+    private  Button ts2;
+    private  Button ts3;
+    private  Button ts4;
+    private  Button ts5;
+    private  Button ts6;
+    private  Button ts7;
+    private  Button ts8;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +32,27 @@ public class TimeSlotActivity extends AppCompatActivity implements View.OnClickL
 
         ts1 = findViewById(R.id.ninetenTS);
         ts1.setOnClickListener(this);
+
+        ts2 = findViewById(R.id.tenelevenTS);
+        ts2.setOnClickListener(this);
+
+        ts3 = findViewById(R.id.eleventwelveTS);
+        ts3.setOnClickListener(this);
+
+        ts4 = findViewById(R.id.twelveoneTS);
+        ts4.setOnClickListener(this);
+
+        ts5 = findViewById(R.id.onetwoTS);
+        ts5.setOnClickListener(this);
+
+        ts6 = findViewById(R.id.twothreeTS);
+        ts6.setOnClickListener(this);
+
+        ts7 = findViewById(R.id.threefourTS);
+        ts7.setOnClickListener(this);
+
+        ts8 = findViewById(R.id.fourfiveTS);
+        ts8.setOnClickListener(this);
     }
 
     @Override
@@ -34,6 +62,30 @@ public class TimeSlotActivity extends AppCompatActivity implements View.OnClickL
             case R.id.ninetenTS:
                 startActivity(intent);
                 break;
+            case R.id.tenelevenTS:
+                startActivity(intent);
+                break;
+
+            case R.id.eleventwelveTS:
+                startActivity(intent);
+                break;
+            case R.id.twelveoneTS:
+                startActivity(intent);
+                break;
+            case R.id.onetwoTS:
+                startActivity(intent);
+                break;
+            case R.id.twothreeTS:
+                startActivity(intent);
+                break;
+            case R.id.threefourTS:
+                startActivity(intent);
+                break;
+            case R.id.fourfiveTS:
+                startActivity(intent);
+                break;
+
+
         }
     }
 }
