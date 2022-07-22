fbe772175457b7dd8cee56b7e17a05ec4d4d67ee
Vineet Sen <imvsen02@gmail.com>
2022-07-22 22:21:56 +0530

    Complete overhaul of the LoginActivity and RegisterActivity

--- app/src/main/java/com/example/roombookingapp/LoginActivity.java
+++ app/src/main/java/com/example/roombookingapp/LoginActivity.java
@@ -6,20 +6,25 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
-    Button cancel;
+    ImageButton backBtn;
     Button login;
+    TextView signupTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        cancel = findViewById(R.id.cancel_1);
+        backBtn = findViewById(R.id.back_login);
         login  = findViewById(R.id.login_2);
-        cancel.setOnClickListener(this);
+        signupTxt = findViewById(R.id.signup_text);
+        backBtn.setOnClickListener(this);
         login.setOnClickListener(this);
+        signupTxt.setOnClickListener(this);
     }
 
     @Override
@@ -29,8 +34,13 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
                 Intent intent1 = new Intent(this, BookingActivity.class);
                 startActivity(intent1);
                 break;
-            case R.id.cancel_1:
+            case R.id.back_login:
                 finish();
+                break;
+            case R.id.signup_text:
+                Intent intent = new Intent(this, RegisterActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
                 break;
         }
 
--- app/src/main/java/com/example/roombookingapp/RegisterActivity.java
+++ app/src/main/java/com/example/roombookingapp/RegisterActivity.java
@@ -7,19 +7,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity implements View.OnClickListener{
-    Button cancelbtn;
+
+    ImageButton backBtn;
     Button registerbtn;
+    TextView loginTxt;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        cancelbtn = findViewById(R.id.cancel_2);
-        cancelbtn.setOnClickListener(this);
+        backBtn = findViewById(R.id.back_register);
+        backBtn.setOnClickListener(this);
         registerbtn = findViewById(R.id.register_2);
         registerbtn.setOnClickListener(this);
+        loginTxt = findViewById(R.id.login_here);
+        loginTxt.setOnClickListener(this);
     }
 
     @Override
@@ -29,9 +36,13 @@ public class RegisterActivity extends AppCompatActivity implements View.OnClickL
                 Intent intent1 = new Intent(this, BookingActivity.class);
                 startActivity(intent1);
                 break;
-            case R.id.cancel_2:
+            case R.id.back_register:
                 finish();
                 break;
+            case R.id.login_here:
+                Intent intent = new Intent(this, LoginActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(intent);
         }
     }
 }
